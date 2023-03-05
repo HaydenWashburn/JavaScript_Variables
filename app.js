8f2beb5ee727c3f371562e690399e340a7ee32d5
HaydenWashburn <hayden.washburn1@gmail.com>
2023-03-05 14:01:12 -0600

    completed exercises 1-5

--- app.js
+++ app.js
@@ -6,5 +6,28 @@ console.log(
 // Exercise 1
 
 // Exercise 2
-
+const firstName="Hayden";
+let lastName="Washburn";
+var age="25";
+console.log(firstName)
+console.log(lastName)
+console.log(age)
 // Exercise 3
+let language="JavaScript";
+let createdYear="1995";
+let isCaseSensitive="true";
+console.log(`${language} was first released in ${createdYear}.`);
+
+let price="19.99";
+let isOnSale="false";
+let salePercentage="15";
+let stock="0";
+let inStock="false";
+let selectedSize="M";
+
+let title="Name of the Wind";
+let author="Patrick Rothfuss";
+let pageCount="722";
+let bookmark="456";
+let hasRead="true";
+console.log(`I have read ${bookmark} out of ${pageCount} pages of ${title} `)
